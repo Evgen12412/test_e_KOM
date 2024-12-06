0dcabb8471a23532c2eabf0d01be7dfb123b8767
evgenijs <evgeny.krd2016@yandex.ru>
2024-12-06 15:18:25 +0300

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+import unittest
+
+from app.main import app
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_get_form(self):
+        response = self.app.post('/get_form', data={'f_name1': 'test@example.com', 'f_name2': '+7 123 456 78 90'})
+        self.assertEqual(response.status_code, 200)
+
+        # Проверяем, что шаблон формы найден
+        if response.json.get("form_name"):
+            self.assertEqual(response.json, {"form_name": "TestForm"})
+        else:
+            self.assertEqual(response.json, {"f_name1": "email", "f_name2": "phone"})
+
+if __name__ == '__main__':
+    unittest.main()
